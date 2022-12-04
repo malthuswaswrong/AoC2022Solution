034ee33567d1c3263e83167ab5e57a78b5f43068
Miller, Michael <MSM@taubman.com>
2022-12-04 08:16:25 -0500

    Refactors

--- AoC2022/Days/Day04.cs
+++ AoC2022/Days/Day04.cs
@@ -10,52 +10,24 @@ public class Day04 : IDay
     {
         _dataLoader = dataLoader ?? new DefaultDataLoader(nameof(Day04));
     }
-    public string SolvePart1(DataSet dataSet)
-    {
-        var dataList = _dataLoader.GetInputList(dataSet);
-        int overlapCount = 0;
-        foreach (var set in dataList)
-        {
-            var segments = set.Split(",");
-            var t1 = segments[0].Split("-").Select(x => int.Parse(x)).ToList();
-            var t2 = segments[1].Split("-").Select(x => int.Parse(x)).ToList();
-            var range1 = new Range(t1[0], t1[1]);
-            var range2 = new Range(t2[0], t2[1]);
-            if (range1.FullOverlap(range2))
-            {
-                overlapCount++;
-            }
-        }
-        return $"{overlapCount}";
-    }
 
-    public string SolvePart2(DataSet dataSet)
+    public string SolvePart1(DataSet dataSet) => $"{_dataLoader.GetInputList(dataSet).Where(x => FullRangeOverlap(GetRanges(x))).Count()}";
+    public string SolvePart2(DataSet dataSet) => $"{_dataLoader.GetInputList(dataSet).Where(x => PartialRangeOverlap(GetRanges(x))).Count()}";
+
+    private (Range, Range) GetRanges(string input)
     {
-        var dataList = _dataLoader.GetInputList(dataSet);
-        int overlapCount = 0;
-        foreach (var set in dataList)
-        {
-            var segments = set.Split(",");
-            var t1 = segments[0].Split("-").Select(x => int.Parse(x)).ToList();
-            var t2 = segments[1].Split("-").Select(x => int.Parse(x)).ToList();
-            var range1 = new Range(t1[0], t1[1]);
-            var range2 = new Range(t2[0], t2[1]);
-            if (range1.PartialOverlap(range2))
-            {
-                overlapCount++;
-            }
-        }
-        return $"{overlapCount}";
+        var segments = input.Split(",");
+        var t1 = segments[0].Split("-").Select(x => int.Parse(x)).ToList();
+        var t2 = segments[1].Split("-").Select(x => int.Parse(x)).ToList();
+        return (new Range(t1[0], t1[1]), new Range(t2[0], t2[1]));
     }
-}
-file static class ElfExtensions
-{
-    public static bool FullOverlap(this Range range1, Range range2)
-    {
-        return (range1.Start.Value <= range2.Start.Value && range1.End.Value >= range2.End.Value) || (range2.Start.Value <= range1.Start.Value && range2.End.Value >= range1.End.Value);
-    }
-    public static bool PartialOverlap(this Range range1, Range range2)
-    {
-        return (range1.Start.Value <= range2.Start.Value && range1.End.Value >= range2.Start.Value) || (range2.Start.Value <= range1.Start.Value && range2.End.Value >= range1.Start.Value);
-    }
+    private bool FullRangeOverlap((Range, Range) set) => Overlap(set.Item1, set.Item2, true);
+    
+    private bool PartialRangeOverlap((Range, Range) set) => Overlap(set.Item1, set.Item2, false);
+    
+    private bool Overlap(Range range1, Range range2, bool full) =>
+        (full)
+        ? (range1.Start.Value <= range2.Start.Value && range1.End.Value >= range2.End.Value) || (range2.Start.Value <= range1.Start.Value && range2.End.Value >= range1.End.Value)
+        : (range1.Start.Value <= range2.Start.Value && range1.End.Value >= range2.Start.Value) || (range2.Start.Value <= range1.Start.Value && range2.End.Value >= range1.Start.Value);
+
 }
